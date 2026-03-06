049977912e1426143b98b6a94abe36fe91938228
kenty <kenta.kgac@gmail.com>
2026-03-07 01:26:47 +0900

    fix:GmailSMTP

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,34 +1,21 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
 
-  # POST /resource/password
-  # def create
-  #   super
-  # end
+    if successfully_sent?(resource)
+      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  rescue StandardError => e
+    Rails.logger.error("[Users::PasswordsController#create] #{e.class}: #{e.message}")
+    Rails.logger.error(e.backtrace.first(10).join("\n")) if e.backtrace
 
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
-
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+    self.resource = resource_class.new
+    flash.now[:alert] = "パスワード再設定メールの送信に失敗しました。SMTP設定を確認してください。"
+    render :new, status: :unprocessable_entity
+  end
 end
